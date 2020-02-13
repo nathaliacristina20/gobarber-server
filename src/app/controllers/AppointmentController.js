369a75f06e9fb01a0b01921272e90b5f501ced0b
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-02-12 21:16:01 -0300

    Listando agendamentos do usuario

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -2,8 +2,34 @@ import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointments';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentControler {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      attributes: ['id', 'date'],
+      order: ['date'],
+      include: {
+        model: User,
+        as: 'provider',
+        attributes: ['id', 'name'],
+        include: [
+          {
+            model: File,
+            as: 'avatar',
+            attributes: ['id', 'path', 'url'],
+          },
+        ],
+      },
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
